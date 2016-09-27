25718c2b06e8a26e4ddd1d91811c0c1b28aafe20
Shwetha <shwetha.ravi3@gmail.com>
2016-09-28 00:25:23 +0800

    LO-DI

--- src/seedu/addressbook/storage/Storage.java
+++ src/seedu/addressbook/storage/Storage.java
@@ -7,7 +7,7 @@ import seedu.addressbook.storage.StorageFile.StorageOperationException;
  * Represents the file used to store address book data.
  */
 public interface Storage {
-    public AddressBook load() throws StorageOperationException;    
-    public void save(AddressBook addressBook) throws StorageOperationException;
+    public AddressBook load() throws StorageOperationException, seedu.addressbook.storage.StorageStub.StorageOperationException;    
+    public void save(AddressBook addressBook) throws StorageOperationException, seedu.addressbook.storage.StorageStub.StorageOperationException;
     public String getPath();
 }
